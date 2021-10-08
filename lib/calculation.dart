40705e1408d9a1cec46ba1414e22cfe1f216ddf1
newt <hi@newty.dev>
2021-10-08 14:17:03 +0100

    Add key functionality

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -8,8 +8,32 @@ class Calculation extends StatefulWidget {
 }
 
 class _CalculationState extends State<Calculation> {
-  int result = 0;
+  int? result;
+  int? firstOperand;
+  int? secondOperand;
+  String? operator;
+
   Color operatorColour = const Color.fromRGBO(220, 220, 220, 1);
+
+  String _getDisplayText() {
+    if (result != null) {
+      return '$result';
+    }
+
+    if (secondOperand != null) {
+      return '$firstOperand$operator$secondOperand';
+    }
+
+    if (operator != null) {
+      return '$firstOperand$operator';
+    }
+
+    if (firstOperand != null) {
+      return '$firstOperand';
+    }
+
+    return '0';
+  }
 
   Widget _getButton(
       {required String text,
@@ -27,36 +51,123 @@ class _CalculationState extends State<Calculation> {
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      ResultDisplay(text: result.toString()),
+      ResultDisplay(text: _getDisplayText()),
       Row(children: [
         _getButton(text: '7', onTap: () => numberPressed(7)),
         _getButton(text: '8', onTap: () => numberPressed(8)),
         _getButton(text: '9', onTap: () => numberPressed(9)),
-        _getButton(text: '*', onTap: () => operatorPressed('*'), backgroundColour: operatorColour)
+        _getButton(
+            text: '*',
+            onTap: () => operatorPressed('*'),
+            backgroundColour: operatorColour)
       ]),
       Row(children: [
         _getButton(text: '4', onTap: () => numberPressed(4)),
         _getButton(text: '5', onTap: () => numberPressed(5)),
         _getButton(text: '6', onTap: () => numberPressed(6)),
-        _getButton(text: '/', onTap: () => operatorPressed('/'), backgroundColour: operatorColour)
+        _getButton(
+            text: '/',
+            onTap: () => operatorPressed('/'),
+            backgroundColour: operatorColour)
       ]),
       Row(children: [
         _getButton(text: '1', onTap: () => numberPressed(1)),
         _getButton(text: '2', onTap: () => numberPressed(2)),
         _getButton(text: '3', onTap: () => numberPressed(3)),
-        _getButton(text: '+', onTap: () => operatorPressed('+'), backgroundColour: operatorColour)
+        _getButton(
+            text: '+',
+            onTap: () => operatorPressed('+'),
+            backgroundColour: operatorColour)
       ]),
       Row(children: [
-        _getButton(text: '=', onTap: calculateResult, backgroundColour: Colors.orange, textColour: Colors.white),
+        _getButton(
+            text: '=',
+            onTap: calculateResult,
+            backgroundColour: Colors.orange,
+            textColour: Colors.white),
         _getButton(text: '0', onTap: () => numberPressed(0)),
         _getButton(text: 'C', onTap: clear, backgroundColour: operatorColour),
-        _getButton(text: '-', onTap: () => operatorPressed('-'), backgroundColour: operatorColour)
+        _getButton(
+            text: '-',
+            onTap: () => operatorPressed('-'),
+            backgroundColour: operatorColour)
       ])
     ]);
   }
 
-  operatorPressed(String operator) {}
-  numberPressed(int number) {}
-  calculateResult() {}
-  clear() {}
+  numberPressed(int number) {
+    setState(() {
+      // If the previous calculation is finished, set the result to null and set the first operand
+      if (result != null) {
+        result = null;
+        firstOperand = number;
+        return;
+      }
+
+      // If the first operand is unset, set it now
+      if (firstOperand == null) {
+        firstOperand = number;
+        return;
+      }
+
+      // If the operator has not been set, pressing a number will concat the number to the first operand. This allows for multi-digit operations.
+      if (firstOperand != null && operator == null) {
+        firstOperand = int.parse('$firstOperand$number');
+        return;
+      }
+
+      // Same logic applies for the second operand
+      if (secondOperand == null) {
+        secondOperand = number;
+        return;
+      }
+
+      secondOperand = int.parse('$secondOperand$number');
+    });
+  }
+
+  operatorPressed(String operator) {
+    firstOperand ??= 0;
+    this.operator = operator;
+  }
+
+  calculateResult() {
+    if (operator == null || secondOperand == null) {
+      return;
+    }
+
+    setState(() {
+      switch (operator) {
+        case '+':
+          result = firstOperand! + secondOperand!;
+          break;
+        case '-':
+          result = firstOperand! - secondOperand!;
+          break;
+        case '*':
+          result = firstOperand! * secondOperand!;
+          break;
+        case '/':
+          if (secondOperand == 0) {
+            return;
+          }
+          result = firstOperand! ~/ secondOperand!;
+          break;
+      }
+
+      firstOperand = result;
+      operator = null;
+      secondOperand = null;
+      result = null;
+    });
+  }
+
+  clear() {
+    setState(() {
+      result = null;
+      operator = null;
+      firstOperand = null;
+      secondOperand = null;
+    });
+  }
 }
